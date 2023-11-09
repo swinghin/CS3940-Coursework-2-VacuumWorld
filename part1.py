da174d4e0bfa481830f31af72f26f1af35f0076b
Wing Hin Shih <Wing.Shih.2020@live.rhul.ac.uk>
2023-11-09 01:28:46 +0000

    Copy example to part 1

---
+++ part1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from typing import Iterable
+
+from vacuumworld import run
+from vacuumworld.model.actions.vwactions import VWAction
+from vacuumworld.model.actions.vwidle_action import VWIdleAction
+from vacuumworld.model.actions.vwbroadcast_action import VWBroadcastAction
+from vacuumworld.model.actions.vweffort import VWActionEffort
+from vacuumworld.model.actor.mind.surrogate.vwactor_mind_surrogate import VWActorMindSurrogate
+
+
+class MyMind(VWActorMindSurrogate):
+    def __init__(self) -> None:
+        super(MyMind, self).__init__()
+
+        # Add here all the attributes you need/want.
+
+    def revise(self) -> None:
+        # Do something with the observation, the messages, and the effort instead of simply storing/printing them.
+
+        print(f"Observation:\n{self.get_latest_observation().pretty_format()}")
+        print(f"Messages: {[str(m) for m in self.get_latest_received_messages()]}")
+        print(f"Current effort since the beginning of the simulation: {self.get_effort()}.")
+
+    def decide(self) -> Iterable[VWAction]:
+        # Replace this trivial decision process with something meaningful.
+        return [VWIdleAction(), VWBroadcastAction(message="Hello!", sender_id=self.get_own_id())]
+
+
+if __name__ == "__main__":
+    run(default_mind=MyMind(), efforts=VWActionEffort.REASONABLE_EFFORTS)
